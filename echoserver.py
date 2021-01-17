9aa05fb5c8af09d2b9d85b75d541491cc81f8e78
hzper <58559245+hzper@users.noreply.github.com>
2021-01-17 16:36:09 +0100

    echoclient i echoserver rade

--- echoserver.py
+++ echoserver.py
@@ -1,3 +1,4 @@
+
 import socket
 
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
@@ -7,10 +8,13 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 
     s.bind((HOST, PORT))
     s.listen()
+
     conn, addr = s.accept()
     with conn:
+
         print('Connected by', addr)
         while True:
+            
             data = conn.recv(1024)
             if not data:
                 break
