00ba9690d0ca02453b8e9333509ee593a0b53a96
hzper <58559245+hzper@users.noreply.github.com>
2021-01-19 21:35:12 +0100

    working one

--- mainclient.py
+++ mainclient.py
@@ -1,62 +1,140 @@
-import socket
-import selectors 
-import types
+# import socket
+# import selectors 
+# import types
 
-HOST = '127.0.0.1'
-PORT = 65432
-num_conns = 2
-messages = [b'Prva poruka s klijenta.', b'Druga poruka s klijenta.']
+# HOST = '127.0.0.1'
+# PORT = 65432
+# num_conns = 2
+# messages = [b'Prva poruka s klijenta.', b'Druga poruka s klijenta.']
 
-sel = selectors.DefaultSelector()
+# sel = selectors.DefaultSelector()
 
-def start_connections (HOST,PORT,num_conns):
-    server_addr = (HOST,PORT)
-    for i in range (0,num_conns):
-        connid = i + 1
-        print("Spajanje ",connid," na ",server_addr)
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.setblocking(False)
-        sock.connect_ex(server_addr)
-        events = selectors.EVENT_READ | selectors.EVENT_WRITE
-        dataVAR = types.SimpleNamespace(connid=connid,
-                msg_total = sum(len(m) for m in messages),
-                recv_total = 0,
-                messages = list(messages),
-                outb=b'')
-        sel.register(sock,events,data= dataVAR)
+# def start_connections(HOST,PORT,num_conns):
 
-def service_connection(key, mask):
-        sock = key.fileobj
-        dataSC = key.data
+#     server_addr = (HOST,PORT)
+    
+#     for i in range (0,num_conns):
+#         connid = i + 1
+#         print("Spajanje",connid,". konekcije na ",server_addr)
+#         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#         sock.setblocking(False)
+#         sock.connect_ex(server_addr)
+#         events = selectors.EVENT_READ | selectors.EVENT_WRITE
+#         dataVAR = types.SimpleNamespace(connid=connid,
+#                 msg_total = sum(len(m) for m in messages),
+#                 recv_total = 0,
+#                 messages = list(messages),
+#                 outb=b'')
 
-        if mask & selectors.EVENT_READ:
-                recv_data = sock.recv(1024) # Should be ready to read
-                if recv_data:
-                        print('Primljeno', repr(recv_data), 'od konekcije',
-                        dataSC.connid)
-                        dataSC.recv_total += len(recv_data)
+#         sel.register(sock,events,data= dataVAR)
 
-                if not recv_data or dataSC.recv_total == dataSC.msg_total:
-                        print('Zatvaranje konekcije ', dataSC.connid)
-                        sel.unregister(sock)
-                        sock.close()
-
-        if mask & selectors.EVENT_WRITE:
-                if not dataSC.outb and dataSC.messages:
-                    dataSC.outb = dataSC.messages.pop(0)        
-                if dataSC.outb:
-                    print('Slanje ', repr(dataSC.outb), 'na konekciju',
-                    dataSC.connid)
-                    sent = sock.send(dataSC.outb)
-                    dataSC.outb = dataSC.outb[sent:]
-
-
-start_connections(HOST,PORT,num_conns)
-
-# while start_connections(HOST,PORT,num_conns):
+#     while True:
 #         events = sel.select(timeout=None)
-#         for key,mask in events:
+#         for key, mask in events:
 #                 if key.data is None:
 #                         pass
 #                 else:
 #                         service_connection(key,mask)
+
+# def service_connection(key, mask):
+
+#         sock = key.fileobj
+#         dataSC = key.data
+
+#         if mask & selectors.EVENT_READ:
+
+#                 recv_data = sock.recv(1024)
+#                 if recv_data:
+#                         print('Primljeno', repr(recv_data), 'od konekcije',
+#                         dataSC.connid)
+#                         dataSC.recv_total += len(recv_data)
+
+#                 if not recv_data or dataSC.recv_total == dataSC.msg_total:
+#                         print('Zatvaranje konekcije ', dataSC.connid)
+#                         sel.unregister(sock)
+#                         sock.close()
+                        
+#         if mask & selectors.EVENT_WRITE:
+
+#                 if not dataSC.outb and dataSC.messages:
+#                         dataSC.outb = dataSC.messages.pop(0)  
+
+#                 if dataSC.outb:
+#                         print('Javljanje ', repr(dataSC.outb), 'na konekciju',
+#                         dataSC.connid)
+#                         sent = sock.send(dataSC.outb)
+#                         dataSC.outb = dataSC.outb[sent:]
+
+
+# start_connections(HOST,PORT,num_conns)
+
+
+import sys
+import socket
+import selectors
+import types
+
+sel = selectors.DefaultSelector()
+messages = [b"Message 1 from client.", b"Message 2 from client."]
+
+
+def start_connections(host, port, num_conns):
+    server_addr = (host, port)
+    for i in range(0, num_conns):
+        connid = i + 1
+        print("starting connection", connid, "to", server_addr)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setblocking(False)
+        sock.connect_ex(server_addr)
+        events = selectors.EVENT_READ | selectors.EVENT_WRITE
+        data = types.SimpleNamespace(
+            connid=connid,
+            msg_total=sum(len(m) for m in messages),
+            recv_total=0,
+            messages=list(messages),
+            outb=b"",
+        )
+        sel.register(sock, events, data=data)
+
+
+def service_connection(key, mask):
+    sock = key.fileobj
+    data = key.data
+    if mask & selectors.EVENT_READ:
+        recv_data = sock.recv(1024)  # Should be ready to read
+        if recv_data:
+            print("received", repr(recv_data), "from connection", data.connid)
+            data.recv_total += len(recv_data)
+        if not recv_data or data.recv_total == data.msg_total:
+            print("closing connection", data.connid)
+            sel.unregister(sock)
+            sock.close()
+    if mask & selectors.EVENT_WRITE:
+        if not data.outb and data.messages:
+            data.outb = data.messages.pop(0)
+        if data.outb:
+            print("sending", repr(data.outb), "to connection", data.connid)
+            sent = sock.send(data.outb)  # Should be ready to write
+            data.outb = data.outb[sent:]
+
+
+if len(sys.argv) != 4:
+    print("usage:", sys.argv[0], "<host> <port> <num_connections>")
+    sys.exit(1)
+
+host, port, num_conns = sys.argv[1:4]
+start_connections(host, int(port), int(num_conns))
+
+try:
+    while True:
+        events = sel.select(timeout=1)
+        if events:
+            for key, mask in events:
+                service_connection(key, mask)
+        # Check for a socket being monitored to continue.
+        if not sel.get_map():
+            break
+except KeyboardInterrupt:
+    print("caught keyboard interrupt, exiting")
+finally:
+    sel.close()
--- mainserver.py
+++ mainserver.py
@@ -1,50 +1,112 @@
+# import socket
+# import selectors
+# import types
+
+# HOST = '127.0.0.1'
+# PORT = 65432
+
+# sel = selectors.DefaultSelector()
+# nb_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+# nb_socket.bind((HOST,PORT))
+# nb_socket.listen()
+
+# print("Slušam na",(HOST,PORT))
+# nb_socket.setblocking(False)
+# sel.register(nb_socket,selectors.EVENT_READ,data=None)
+
+# def accept_wrapper(sock):
+
+#     conn,addr = sock.accept()
+#     print("Prihvaćena konekcija od ",addr)
+#     conn.setblocking(False)
+#     data = types.SimpleNamespace(addr=addr,inb=b'',outb=b'')
+#     events = selectors.EVENT_READ | selectors.EVENT_WRITE
+
+#     sel.register(conn,events,data=data)
+
+# def service_connection(key,mask):
+#     sock = key.fileobj
+#     data = key.data
+#     if mask & selectors.EVENT_READ:
+#         recv_data = sock.recv(1024) # vraća primljene podatke
+#         if recv_data:
+#             data.outb += recv_data
+#         else:
+#             print("Zatvaranje konekcije za ",data.addr)
+#             sel.unregister(sock)
+#             sock.close()
+
+#     if mask & selectors.EVENT_WRITE:
+#         if data.outb:
+#             print("S KLIJENTA: ",repr(data.outb),'-----',data.addr)
+#             sent = sock.send(data.outb) #vraća broj poslanih bajtova
+#             data.outb = data.outb[sent:]
+
+# while True:
+#     events = sel.select(timeout=None)
+#     for key, mask in events:
+#         if key.data is None:
+#             accept_wrapper(key.fileobj)
+#         else:
+#             service_connection(key,mask)
+
+
+import sys
 import socket
 import selectors
 import types
 
-HOST = '127.0.0.1'
-PORT = 65432
-
 sel = selectors.DefaultSelector()
-nb_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-nb_socket.bind((HOST,PORT))
-nb_socket.listen()
 
-print("Slusam na",(HOST,PORT))
-nb_socket.setblocking(False)
-sel.register(nb_socket,selectors.EVENT_READ,data=None)
 
 def accept_wrapper(sock):
-
-    conn,addr = sock.accept()
-    print("Prihvaćena konekcija od: ",addr)
+    conn, addr = sock.accept()  # Should be ready to read
+    print("accepted connection from", addr)
     conn.setblocking(False)
-    data = types.SimpleNamespace(addr=addr,inb=b'',outb=b'')
+    data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
     events = selectors.EVENT_READ | selectors.EVENT_WRITE
-    sel.register(conn,events,data=data)
+    sel.register(conn, events, data=data)
 
-def service_connection(key,mask):
+
+def service_connection(key, mask):
     sock = key.fileobj
     data = key.data
     if mask & selectors.EVENT_READ:
-        recv_data = sock.recv(1024) # vraća primljene podatke
+        recv_data = sock.recv(1024)  # Should be ready to read
         if recv_data:
             data.outb += recv_data
         else:
-            print("Zatvaranje konekcije za: ",data.addr)
+            print("closing connection to", data.addr)
             sel.unregister(sock)
             sock.close()
-
     if mask & selectors.EVENT_WRITE:
         if data.outb:
-            print("Echoing",repr(data.outb),'to',data.addr)
-            sent = sock.send(data.outb) #vraća broj poslanih bajtova
+            print("echoing", repr(data.outb), "to", data.addr)
+            sent = sock.send(data.outb)  # Should be ready to write
             data.outb = data.outb[sent:]
 
-while True:
-    events = sel.select(timeout=None)
-    for key, mask in events:
-        if key.data is None:
-            accept_wrapper(key.fileobj)
-        else:
-            service_connection(key,mask)
+
+if len(sys.argv) != 3:
+    print("usage:", sys.argv[0], "<host> <port>")
+    sys.exit(1)
+
+host, port = sys.argv[1], int(sys.argv[2])
+lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+lsock.bind((host, port))
+lsock.listen()
+print("listening on", (host, port))
+lsock.setblocking(False)
+sel.register(lsock, selectors.EVENT_READ, data=None)
+
+try:
+    while True:
+        events = sel.select(timeout=None)
+        for key, mask in events:
+            if key.data is None:
+                accept_wrapper(key.fileobj)
+            else:
+                service_connection(key, mask)
+except KeyboardInterrupt:
+    print("caught keyboard interrupt, exiting")
+finally:
+    sel.close()
